e83b98aa1f17dd83be0d42ea180f323c0f1dad5d
Vignesh <svzvignesh@hotmail.com>
2026-04-04 20:23:29 +0530

    Initial commit for backend

---
+++ roster-backend/src/main/java/com/vignesh/roster/backend/RosterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vignesh.roster.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RosterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RosterBackendApplication.class, args);
+	}
+
+}
---
+++ roster-backend/src/test/java/com/vignesh/roster/backend/RosterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vignesh.roster.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RosterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
